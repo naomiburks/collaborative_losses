7c175dc85e1b778f9e92c7738b377b579686f73d
B Burks <33607825+bburks@users.noreply.github.com>
2023-12-29 17:57:48 -0800

    added or filter

--- data_cleaner.py
+++ data_cleaner.py
@@ -36,8 +36,25 @@ class AndFilter(Filter):
                 return False
         return True
 
+class OrFilter(Filter):
+    def __init__(self, filters):
+        self.filters = filters
+        super().__init__()
+
+    def validate(self, data):
+        for data_filter in self.filters:
+            if data_filter.validate(data):
+                return True
+        return False
+    
+
 def make_filter(player_count, variant):
     player_count_filter = ExactItemFilter("num_players", player_count)
     variant_filter = ExactItemFilter("variant_id", variant)
     return AndFilter([player_count_filter, variant_filter])
 
+def variant_filter(allowed_variants):
+    filters = []
+    for variant in allowed_variants:
+        filters.append(ExactItemFilter("variant_id", variant))
+    return OrFilter(filters)
