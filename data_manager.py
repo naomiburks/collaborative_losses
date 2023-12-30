35e5f14bf42a121aafb7cb227910e1fd9dac4d55
B Burks <33607825+bburks@users.noreply.github.com>
2023-12-29 17:57:09 -0800

    can fetch from league site

--- data_manager.py
+++ data_manager.py
@@ -1,9 +1,20 @@
 import json
+import requests
+import sys
 
-league_filepath = "league_data.json"
 
+LEAGUE_FILEPATH = "league_data.json"
+LEAGUE_URL = "https://hanabi-league.com/api/export/games/"
 
 def extract_league_data():
-    with open(league_filepath) as league_file:
+    with open(LEAGUE_FILEPATH) as league_file:
         league_data = json.load(league_file)
     return league_data
+
+def request_league_data():
+    result = requests.get(LEAGUE_URL, timeout=10)
+    with open(LEAGUE_FILEPATH, "w") as league_file:
+        json.dump(result.json(), league_file)
+
+if __name__ == "__main__":
+    request_league_data()
